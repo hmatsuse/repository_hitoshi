37dae5504835fb4548b1e36b16d40b629f6efd63
Hitoshi Matsuse <hmatsuse@c1r20s1.42tokyo.jp>
2020-07-02 13:51:17 +0900

    Libft Finished

--- 42/project/libft/ft_lstmap.c
+++ 42/project/libft/ft_lstmap.c
@@ -6,37 +6,62 @@
 /*   By: hmatsuse <hmatsuse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 21:46:37 by hmatsuse          #+#    #+#             */
-/*   Updated: 2020/07/01 22:14:56 by hmatsuse         ###   ########.fr       */
+/*   Updated: 2020/07/02 13:47:52 by hmatsuse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"	
+#include "libft.h"
 
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*tmp;
+	t_list	*newlst;
 	t_list	*list;
-	t_list	*s;
-	size_t	size;
 
-	if (!lst)
-		return (malloc(0));
-	list = lst;
-	while (lst)
+	if (!lst || !f)
+		return (NULL);
+	if (!(newlst = ft_lstnew(f(lst->content))))
 	{
-		size = ft_strlen(lst->content) + 1;
-		if (!(tmp = malloc(sizeof(void *) * size)))
+		ft_lstclear(&newlst, del);
+		return (NULL);
+	}
+	list = newlst;
+
+	while (lst->next)
+	{
+		if (!(list->next = ft_lstnew(f(lst->next->content))))
+		{
+			ft_lstclear(&newlst, del);
 			return (NULL);
-		tmp = f(lst->content);
+		}
 		lst = lst->next;
+		list = list->next;
 	}
-	while (list)
-	{
-		s = list;
-		del(list->content);
-		free(list);
-		list = s->content;
-	}
-	list = NULL;
-	return (tmp);
+	return (newlst);
 }
+
+// t_list	*newlist;
+// 	t_list	*list;
+// 	size_t	size;
+// 	size_t	i;
+
+// 	size = 0;
+// 	i = 0;
+// 	tmp = NULL;
+// 	while (lst)
+// 	{
+// 		size = ft_strlen(lst->content);
+// 		if (!(tmp = malloc(sizeof(void *) * size + 1)))
+// 		{
+// 			while (1)
+// 			{
+// 				del(lst->content);
+// 				free(tmp);
+// 			}
+// 			return (NULL);
+// 		}
+// 		tmp = f(lst->content);
+// 		if (i++ == 0)
+// 			list = tmp;
+// 		lst = lst->content;
+// 	}
+// 	return (list);
--- 42/project/libft/libft.h
+++ 42/project/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hmatsuse <hmatsuse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 17:08:17 by hmatsuse          #+#    #+#             */
-/*   Updated: 2020/07/01 21:47:35 by hmatsuse         ###   ########.fr       */
+/*   Updated: 2020/07/02 13:03:19 by hmatsuse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	t_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
