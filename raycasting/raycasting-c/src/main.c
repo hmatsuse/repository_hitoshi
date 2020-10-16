c5af3a01add92a3b96284b51a8925b6fbc845a66
Hitoshi Matsuse <hmatsuse@c1r3s1.42tokyo.jp>
2020-10-16 22:42:46 +0900

    raycast c

---
+++ raycasting/raycasting-c/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void initializeWindow() {
+    
+}
+
+int     main() {
+    initializeWindow();
+    
+    while (1) {
+        // processInput();
+        // update();
+        // render();
+    }
+    return 0;
+};
